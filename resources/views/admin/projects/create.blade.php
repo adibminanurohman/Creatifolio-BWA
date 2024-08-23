e3753d4e9ef22fd3b98e739167f267523f893008
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-23 11:18:00 +0700

    membuat form create project

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -6,12 +6,45 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-28">
-                <a href="{{ route('admin.projects.create') }}"
-                    class="py-4 px-10 text-white bg-indigo bg-indigo-950 rounded-full font-bold">
-                    Add New Projects
-                </a>
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
+                <form action="">
+                    <div class="flex flex-col gap-y-5">
+                        <h1 class="text-3xl text-indigo-950 font-bold">
+                            Add New Project
+                        </h1>
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Name
+                            </h3>
+                            <input type="text" id="name" name="name">
+                        </div>
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Category
+                            </h3>
+                            <select name="category" id="category">
+                                <option value="">Coose Category below</option>
+                                <option value="Website Development">Website Development</option>
+                                <option value="App Development">App Development</option>
+                                <option value="UI/UX Design">UI/UX Design</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Cover Image
+                            </h3>
+                            <input type="file" id="cover" name="cover">
+                        </div>
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                About
+                            </h3>
+                            <textarea name="about" id="about" cols="30" rows="10"></textarea>
+                        </div>
+                        <button type="submit" class="py-4 w-full rounded-full bg-violet-700 font-bold text-white">Upload Project</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
