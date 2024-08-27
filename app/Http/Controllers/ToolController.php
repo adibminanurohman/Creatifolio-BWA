13dd29af0f9bea1a85700ac9bdf15adb269e9006
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-27 21:38:41 +0700

    coding fitur add new tool

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tool;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ToolController extends Controller
 {
@@ -12,7 +14,10 @@ class ToolController extends Controller
      */
     public function index()
     {
-        dd('hello');
+        $tools = Tool::orderBy('id', 'desc')->get();
+        return view('admin.tools.index', [
+            'tools' => $tools
+        ]);
     }
 
     /**
@@ -20,7 +25,7 @@ class ToolController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.tools.create');
     }
 
     /**
@@ -28,7 +33,27 @@ class ToolController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'tagline' => 'required|string|max:255',
+            'logo' => 'required|image|mimes:png,jpg|max:2048',
+        ]);
+
+
+        DB::beginTransaction();
+
+        try {
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('tools', 'public');
+                $validated['logo'] = $path;
+            }
+            $newTool = Tool::create($validated);
+            DB::commit();
+            return redirect()->route('admin.tools.index')->with('success', 'Tool created succesfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'System error!' . $e->getMessage());
+        }
     }
 
     /**
@@ -60,6 +85,12 @@ class ToolController extends Controller
      */
     public function destroy(Tool $tool)
     {
-        //
+        try {
+            $tool->delete();
+            return redirect()->back()->with('success', 'Tool delete succesfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'System error!' . $e->getMessage());
+        }
     }
 }
